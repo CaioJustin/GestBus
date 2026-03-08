430a7bacf90b1c3ef97f15a264e3b73dda59f392
Caio justino <caio.laife@gmail.com>
2026-03-08 17:30:24 -0300

    Adicionando Botões na janela principal

--- src/main/java/br/com/Main.java
+++ src/main/java/br/com/Main.java
@@ -1,5 +1,7 @@
 package br.com;
 
+import javax.swing.UIManager;
+
 import br.com.view.JanelaPrincipal;
 
 
@@ -7,6 +9,24 @@ public class Main {
 
     public static void main(String[] args) {
         
+                try {
+            // Tenta configurar o Look and Feel Nimbus
+            for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            // Se não conseguir configurar o Nimbus, usa o Look and Feel padrão do sistema
+            try {
+                UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+
+
         JanelaPrincipal window = new JanelaPrincipal();
         window.setVisible(true);
 
--- src/main/java/br/com/view/JanelaPrincipal.java
+++ src/main/java/br/com/view/JanelaPrincipal.java
@@ -2,7 +2,10 @@ package br.com.view;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.Font;
+
 import javax.swing.*;
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
@@ -26,10 +29,97 @@ public class JanelaPrincipal extends JFrame {
 
     private void CriarPanel(){
         JPanel painel = new JPanel();
-        painel.setLayout(new FlowLayout(FlowLayout.CENTER));
+        painel.setLayout(new BoxLayout(painel, BoxLayout.Y_AXIS));
         add(painel,BorderLayout.WEST);
-        painel.setBackground(Color.red);
+        painel.setBackground(Color.BLUE);
+        CriandoBotoes(painel);
     }
 
+    private void CriandoBotoes(JPanel painel){
+        JButton BFuncionario= new JButton("Funcionario");
+        JButton BLinhas =new JButton("Linhas");
+        JButton BOnibus= new JButton("Onibus");
+        JButton BCor =new JButton("Status");
+        JButton BCargo=new JButton("Cargo");
+        JButton BEscala =new JButton("Escala");
+        JButton BGaragem=new JButton("Garagem");
+        JButton BViagem =new JButton("Viagem");
+        JButton BSair=new JButton("Sair");
+
+        //Modificando Botões 
+
+      
+        BFuncionario.setHorizontalAlignment(SwingConstants.CENTER);
+        BFuncionario.setContentAreaFilled(false);
+        BFuncionario.setOpaque(false);
+        BFuncionario.setFont(new Font("Arial",Font.BOLD,20));
+        BFuncionario.setForeground(Color.WHITE);
+
+        BLinhas.setHorizontalAlignment(SwingConstants.CENTER);
+        BLinhas.setContentAreaFilled(false);
+        BLinhas.setOpaque(false);
+        BLinhas.setFont(new Font("Arial",Font.BOLD,20));
+        BLinhas.setForeground(Color.WHITE);
+
+        BOnibus.setHorizontalAlignment(SwingConstants.CENTER);
+        BOnibus.setContentAreaFilled(false);
+        BOnibus.setOpaque(false);
+        BOnibus.setFont(new Font("Arial",Font.BOLD,20));
+        BOnibus.setForeground(Color.WHITE);
+
+        BCargo.setHorizontalAlignment(SwingConstants.CENTER);
+        BCargo.setContentAreaFilled(false);
+        BCargo.setOpaque(false);
+        BCargo.setFont(new Font("Arial",Font.BOLD,20));
+        BCargo.setForeground(Color.WHITE);
+
+        BCor.setHorizontalAlignment(SwingConstants.CENTER);
+        BCor.setContentAreaFilled(false);
+        BCor.setOpaque(false);
+        BCor.setFont(new Font("Arial",Font.BOLD,20));
+        BCor.setForeground(Color.WHITE);
+
+        BEscala.setHorizontalAlignment(SwingConstants.CENTER);
+        BEscala.setContentAreaFilled(false);
+        BEscala.setOpaque(false);
+        BEscala.setFont(new Font("Arial",Font.BOLD,20));
+        BEscala.setForeground(Color.WHITE);
+
+        BGaragem.setHorizontalAlignment(SwingConstants.CENTER);
+        BGaragem.setContentAreaFilled(false);
+        BGaragem.setOpaque(false);
+        BGaragem.setFont(new Font("Arial",Font.BOLD,20));
+        BGaragem.setForeground(Color.WHITE);
+
+        BViagem.setHorizontalAlignment(SwingConstants.CENTER);
+        BViagem.setContentAreaFilled(false);
+        BViagem.setOpaque(false);
+        BViagem.setFont(new Font("Arial",Font.BOLD,20));
+        BViagem.setForeground(Color.WHITE);
+
+        BSair.setHorizontalAlignment(SwingConstants.CENTER);
+        BSair.setContentAreaFilled(false);
+        BSair.setOpaque(false);
+        BSair.setFont(new Font("Arial",Font.BOLD,20));
+        BSair.setForeground(Color.WHITE);
+
+
+        //Adicionanado Botão
+        painel.add(BFuncionario);
+        painel.add(BLinhas);   
+        painel.add(BOnibus);   
+        painel.add(BCor);   
+        painel.add(BCargo); 
+        painel.add(BEscala);      
+        painel.add(BGaragem);    
+        painel.add(BViagem);      
+        painel.add(BSair);
+       
+
+        
+
+
+        
+    }
 
 }
